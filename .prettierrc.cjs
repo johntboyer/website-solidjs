100ec31ad5d293e420d123460ca0cb3644b58630
John Boyer <118901899+johntboyer@users.noreply.github.com>
2023-06-11 10:13:56 +1000

    Configured prettier, eslint and vscode and format

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  editorConfig: true,
+	editorConfig: true
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
